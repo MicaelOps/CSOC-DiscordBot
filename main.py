f46222f3bbb9ca6e67c484cc1d685edce67a3639
MicaelOps <jthansmith55@gmail.com>
2024-02-18 08:55:11 +0000

    Discord_handler Introducted

--- main.py
+++ main.py
@@ -2,14 +2,24 @@ import discord
 
 TOKEN_FILE = "token.txt"
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+discord_handler = discord.Client(intents=intents)
+
 
 # Reads the token from file
 def readDiscordToken():
-    return ""
+
+    fo = open(TOKEN_FILE, "r")
+    token = fo.read(72)
+    fo.close()
+
+    return token
 
 
 def loadBot():
-    token = readDiscordToken()
+    discord_handler.run(readDiscordToken())
 
 
 # Press the green button in the gutter to run the script.
