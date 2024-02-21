e93b4b99e568d9d39d29b727231331532de563ea
MicaelOps <jthansmith55@gmail.com>
2024-02-21 18:38:45 +0000

    Setting up extension system

--- main.py
+++ main.py
@@ -1,25 +1,51 @@
 import discord
+import os
 
+from discord.ext import commands
+
+# File where it contains private Discord Token
 TOKEN_FILE = "token.txt"
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-discord_handler = discord.Client(intents=intents)
+discord_handler = commands.Bot(command_prefix="/", intents=intents)
 
 
-# Reads the token from file
+# Reads the 72 character token from file
 def readDiscordToken():
-
     fo = open(TOKEN_FILE, "r")
-    token = fo.read(72) # Size of token
+    token = fo.read(72)
     fo.close()
 
     return token
 
 
+# Looks through all the files in
+# the extensions folder and returns the list of python files.
+def getAllExtensions():
+    return filter(lambda files: files.endswith('.py'), os.listdir('./extensions'))
+
+
+# Loads all the extensions from extensions folder
+def loadAllExtensions():
+    for ext in getAllExtensions():
+        discord_handler.load_extension(f'extensions.{ext[:-3]}')
+
+
+# Unloads all the extensions from extensions folder
+def unloadAllExtensions():
+    for ext in getAllExtensions():
+        discord_handler.unload_extension(f'extensions.{ext[:-3]}')
+
+
+# Starts up the Discord bot and extensions
 def loadBot():
     discord_handler.run(readDiscordToken())
+
+    loadAllExtensions()
+
+    print('Bot loaded successfully!')
 
 
 if __name__ == '__main__':
