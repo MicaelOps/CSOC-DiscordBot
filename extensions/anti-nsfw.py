e50d4392cb128dfaf78f1e98994f9c420ae55177
MicaelOps <jthansmith55@gmail.com>
2024-02-26 13:17:17 +0000

    Message listener added

--- extensions/anti-nsfw.py
+++ extensions/anti-nsfw.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from discord.ext.commands import bot
 
 
 @commands.command()
@@ -6,6 +7,15 @@ async def hello(ctx):
     await ctx.send(f'Hello 2.0 {ctx.author.display_name}.')
 
 
+async def nsfw_message_listener(message):
+    message.edit('none')
+
+
+# Extension init required as per documentation
+# https://discordpy.readthedocs.io/en/stable/ext/commands/extensions.html
 async def setup(bot):
-    print('I am being loaded!')
+    print('NSFW extension loaded.')
+
     bot.add_command(hello)
+
+    bot.add_listener(nsfw_message_listener, 'on_message')
