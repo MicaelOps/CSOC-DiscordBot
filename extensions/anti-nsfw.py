9c0f2cc972c7bdab832b8728803b7cad2f0c54d6
MicaelOps <jthansmith55@gmail.com>
2024-02-26 13:33:06 +0000

    Message listener added

--- extensions/anti-nsfw.py
+++ extensions/anti-nsfw.py
@@ -8,7 +8,8 @@ async def hello(ctx):
 
 
 async def nsfw_message_listener(message):
-    await message.channel.send(f'this message has {len(message.embeds)} embeds')
+    if message.author.id == 629349854950457355:
+        await message.channel.send(f'this message has {len(message.embeds)} embeds')
 
 # Extension init required as per documentation
 # https://discordpy.readthedocs.io/en/stable/ext/commands/extensions.html
