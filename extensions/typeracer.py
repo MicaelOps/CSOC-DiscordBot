b849b3e886933450fd644beabd006ca4fff00698
MicaelOps <jthansmith55@gmail.com>
2024-02-29 18:47:00 +0000

    Typer racer Leaderboard introduction

--- extensions/typeracer.py
+++ extensions/typeracer.py
@@ -41,18 +41,7 @@ async def showLeaderboard(ctx):
     main_embed = discord.Embed(colour=discord.Colour.from_rgb(241, 196, 15))
     main_embed.set_author(name='TOP 10 Type racer', url='https://github.com/MicaelOps/CSOC-DiscordBot')
 
-    top_ten_text = ''
-    top_ten_count = 10
-
-    for racer_index in range(len(top_ten)):
-        top_ten_text.join(f'TOP{racer_index + 1} {top_ten[racer_index][0]} - {top_ten[racer_index][1]} WPM \n')
-        top_ten_count = top_ten_count - 1
-
-    # Fill the remainder positions in the Top 10
-    for racer_index in range(len(top_ten_count)):
-        top_ten_text.join(f'TOP{racer_index + 1} ######## - 0 WPM \n')
-
-    main_embed.add_field(name='top10', value=top_ten_text, inline=True)
+    main_embed.add_field(name='top10', value="top_ten_text", inline=True)
 
     # Avoid mention of top 10 usernames from becoming a notification
     await ctx.channel.send(silent=True, embed=main_embed)
